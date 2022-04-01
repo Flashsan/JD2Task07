4822b519344e5a3253a8be90c8f3f99e512fc5d8
Alexander Grigorovich <algrigr@gmail.com>
2022-04-01 03:20:39 +0300

    liquibase

--- src/main/java/by/academy/it/task07/entity/Person.java
+++ src/main/java/by/academy/it/task07/entity/Person.java
@@ -32,7 +32,7 @@ public class Person {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) return false;//////
         Person person = (Person) o;
         return Objects.equals(id, person.id) &&
                 Objects.equals(surname, person.surname) &&
@@ -42,7 +42,7 @@ public class Person {
     @Override
     public int hashCode() {
         return Objects.hash(id, surname, name);
-    }
+    }///////
 
     @Override
     public String toString() {
